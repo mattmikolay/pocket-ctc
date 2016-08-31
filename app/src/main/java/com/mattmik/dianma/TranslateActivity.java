df3517411998089df7c0ab2f1f4c51b1870c662a
Matthew Mikolay <mikolaym@yahoo.com>
2016-08-31 17:43:16 -0400

    Add app bar to TranslateActivity

--- app/src/main/java/com/mattmik/dianma/TranslateActivity.java
+++ app/src/main/java/com/mattmik/dianma/TranslateActivity.java
@@ -1,7 +1,11 @@
 package com.mattmik.dianma;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 // Copyright 2016 Matthew Mikolay. All rights reserved.
 
@@ -9,8 +13,48 @@ public class TranslateActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_translate);
+
+        // Set up app bar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(myToolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.action_settings:
+                launchSettingsActivity();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+
+    /**
+     * Launches a new instance of {@link SettingsActivity}.
+     */
+    private void launchSettingsActivity() {
+
+        Intent launchIntent = new Intent(this, SettingsActivity.class);
+        startActivity(launchIntent);
+
     }
 
 }
