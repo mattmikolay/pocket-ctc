23a8b0eb70dbdaf03a560e6327fddd56beb32dcb
Matthew Mikolay <mikolaym@yahoo.com>
2016-08-30 21:41:49 -0400

    Add TranslateActivity

---
+++ app/src/main/java/com/mattmik/dianma/TranslateActivity.java
@@ -0,0 +1,16 @@
+package com.mattmik.dianma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// Copyright 2016 Matthew Mikolay. All rights reserved.
+
+public class TranslateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translate);
+    }
+
+}
