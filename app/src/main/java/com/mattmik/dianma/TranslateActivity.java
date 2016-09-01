218e6c38490e97b565deed968cc904d27df14484
Matthew Mikolay <mikolaym@yahoo.com>
2016-09-01 16:28:09 -0400

    Add TextWatcher to main EditText
    This TextWatcher will be used to trigger the translation of a given
    text whenever the user pauses after typing in the EditText.

--- app/src/main/java/com/mattmik/dianma/TranslateActivity.java
+++ app/src/main/java/com/mattmik/dianma/TranslateActivity.java
@@ -1,15 +1,39 @@
 package com.mattmik.dianma;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 // Copyright 2016 Matthew Mikolay. All rights reserved.
 
 public class TranslateActivity extends AppCompatActivity {
+
+    private final TextWatcher mInputWatcher = new TextWatcher() {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            // Empty!
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            // TODO Translate given text s
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            // Empty!
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +44,18 @@ public class TranslateActivity extends AppCompatActivity {
         // Set up app bar
         Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(myToolbar);
+
+        // Add a TextWatcher to this Activity's main EditText, allowing us to detect when the user
+        // stops typing
+        EditText inputText = (EditText) findViewById(R.id.txt_input);
+        inputText.addTextChangedListener(mInputWatcher);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
 
     }
 
