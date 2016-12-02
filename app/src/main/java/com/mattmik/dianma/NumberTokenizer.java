93ac1e043e421e47bbfb86a9382f8903ba5f95b4
Matthew Mikolay <mikolaym@yahoo.com>
2016-12-01 19:58:49 -0500

    Add detailed copyright notice to Java source files

--- app/src/main/java/com/mattmik/dianma/NumberTokenizer.java
+++ app/src/main/java/com/mattmik/dianma/NumberTokenizer.java
@@ -1,6 +1,22 @@
+/*
+ * Copyright (c) 2016 Matthew Mikolay
+ *
+ * This file is part of Pocket CTC.
+ *
+ * Pocket CTC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pocket CTC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Pocket CTC.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.mattmik.dianma;
-
-// Copyright 2016 Matthew Mikolay. All rights reserved.
 
 import java.util.NoSuchElementException;
 
--- app/src/main/java/com/mattmik/dianma/TranslateActivity.java
+++ app/src/main/java/com/mattmik/dianma/TranslateActivity.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2016 Matthew Mikolay
+ *
+ * This file is part of Pocket CTC.
+ *
+ * Pocket CTC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pocket CTC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Pocket CTC.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.mattmik.dianma;
 
 import android.content.Intent;
@@ -15,8 +33,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
-
-// Copyright 2016 Matthew Mikolay. All rights reserved.
 
 public class TranslateActivity extends AppCompatActivity
         implements SwitcherView.OnTranslateModeSwitchListener {
